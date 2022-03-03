28ddf7e64becc8bb562884e58390e145083ea45f
borkab <borbala.botond@gmail.com>
2022-03-03 23:25:08 +0100

    refactored for tests

--- countthecharacters/main.go
+++ countthecharacters/main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"unicode/utf8"
 )
 
 func main() {
+	countRunes()
+	countChars()
+}
+
+func countChars() int {
 	// Currently it returns 7
 	// Because, it counts the bytes...
 	// It should count the runes (codepoints) instead.
 	//
 	// When you run it with "borka", it should return 5 not 7.
 	length := len(os.Args[1])
-	fmt.Println(length)
+	return length
 
 	//but it doesn't return 7 as written in the exercise
 	//the output is correct, 5
-	countRunes()
 }
-func countRunes() {
+
+func countRunes() int {
 
 	lenght := utf8.RuneCountInString(os.Args[1])
-	fmt.Println(lenght)
+	return lenght
 }
