e6dff864719875a77d544de50611c85a6ab11eff
borkab <borbala.botond@gmail.com>
2022-03-03 22:43:56 +0100

    comment for output

--- countthecharacters/main.go
+++ countthecharacters/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-
+	// Currently it returns 7
+	// Because, it counts the bytes...
+	// It should count the runes (codepoints) instead.
+	//
+	// When you run it with "borka", it should return 5 not 7.
 	length := len(os.Args[1])
 	fmt.Println(length)
 }
