9df53dd55fcccfbf53c29775040817f6abf5c27f
borkab <borbala.botond@gmail.com>
2022-03-04 23:34:08 +0100

    refresh main_test.go

--- banger/main_test.go
+++ banger/main_test.go
@@ -21,15 +21,3 @@ func TestBangerUni(t *testing.T) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
-
-/*
-[borkab@severest banger]$ go test -v
-# learngo/github/learngo/gitto/banger [learngo/github/learngo/gitto/banger.test]
-./main_test.go:7:15: too many arguments in call to banger
-        have (string)
-        want ()
-./main_test.go:18:18: too many arguments in call to bangerUni
-        have (string)
-        want ()
-FAIL    learngo/github/learngo/gitto/banger [build failed]
-*/
