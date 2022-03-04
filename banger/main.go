684ccc0442000c90462b2a9fde3f205d1dce8329
borkab <borbala.botond@gmail.com>
2022-03-04 23:31:27 +0100

    refresh main.go

--- banger/main.go
+++ banger/main.go
@@ -8,21 +8,19 @@ import (
 )
 
 func main() {
-	//change the Banger program to work with unicode characters
-	//input: miniponi
-	//output:miniponi!!!!!!!!
-	fmt.Println(banger())
-	fmt.Println(bangerUni())
+	msg := os.Args[1]
+	fmt.Println(banger(msg))
+	fmt.Println(bangerUni(msg))
 }
 
-func banger() string {
-	msg := os.Args[1]
+func banger(msg string) string {
+
 	s := msg + strings.Repeat("!", len(msg))
 
 	return s
 }
-func bangerUni() string {
-	msg := os.Args[1]
+func bangerUni(msg string) string {
+
 	l := utf8.RuneCountInString(msg)
 	s := msg + strings.Repeat("!", l)
 
