f013ae53df7606ff8ddf52b0f01c5ff734c2e443
borkab <borbala.botond@gmail.com>
2022-03-04 08:28:36 +0100

    change t to T in func Test~

--- countthecharacters/main_test.go
+++ countthecharacters/main_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func testMain(t *testing.T) {
+func TestMain(t *testing.T) {
 
 	got := countChars()
 	want := 5 //input:borka
@@ -13,7 +13,7 @@ func testMain(t *testing.T) {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
-func testCountRunes(t *testing.T) {
+func TestCountRunes(t *testing.T) {
 
 	got := countRunes()
 	want := 5 //input:borka
@@ -23,5 +23,11 @@ func testCountRunes(t *testing.T) {
 
 }
 
-//output: pass ok
-//but warning: no tests to run
+/*--- FAIL: TestMain (0.00s)
+    main_test.go:13: got 18 want 5
+--- FAIL: TestCountRunes (0.00s)
+    main_test.go:21: got 18 want 5
+FAIL
+exit status 1
+FAIL    learngo/github/learngo/gitto/countthecharacters 0.003s
+*/
