43b4bdb5d7f239333029e318316aa485b5cd414c
borkab <borbala.botond@gmail.com>
2022-03-05 17:22:57 +0100

    main.go is good but I can't write my test for it

--- right-trim-it/main.go
+++ right-trim-it/main.go
@@ -6,11 +6,23 @@ import (
 )
 
 func main() {
-	name := "tunyacsap"
-	fmt.Println(rightTrimIt(name))
+	m := "tunyacsap             "
+	fmt.Println(lenSplit(Split((rightTrimIt(m)), "")))
+	//fmt.Println(Split((rightTrimIt(m)), ""))
 }
 
 func rightTrimIt(name string) string {
 	s := strings.TrimRight(name, " ")
 	return s
+}
+
+func Split(s string, sep string) []string {
+
+	sp := strings.Split(s, "")
+	return sp
+}
+
+func lenSplit(sp []string) int {
+	ls := len(sp)
+	return ls
 }
--- right-trim-it/main_test.go
+++ right-trim-it/main_test.go
@@ -12,3 +12,24 @@ func TestRighgtTrimIt(t *testing.T) {
 	}
 
 }
+
+func TestSplit(t *testing.T) {
+	n := "milka"
+	got := Split(n, "")
+	want := []string{m, i, l, k, a} // [m i l k a] hogy irjam be h ezt elfogadja?
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestLenSplit(t *testing.T) {
+	n := []string{m, i, l, k, a} //hogy irjam be a slice-t?
+	got := lenSplit(n)
+	want := 5
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+
+}
