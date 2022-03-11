7aeda3ff50d6ae7ad95168e3c678ee60ba0f5893
borkab <borbala.botond@gmail.com>
2022-03-11 14:26:54 +0100

    excercise

---
+++ types/typeProblem/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/*exercise: solve the data type problem in the program
+
+expected output:
+width: 265 height: 265
+are they equal? true
+*/
+
+func main() {
+	//change the  following data types to the correct
+	//data types where appropriate.
+	var (
+		width  uint8
+		height uint16
+	)
+
+	//don't touch this:
+	width, height = 255, 265
+	width += 10
+	fmt.Printf("width: %d height: %d\n", width, height)
+}
