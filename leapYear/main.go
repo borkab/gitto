cd35992af81440fcad2296f63a453d29d29ed449
borkab <borbala.botond@gmail.com>
2022-04-25 09:13:43 +0200

    simplify if statement

--- leapYear/main.go
+++ leapYear/main.go
@@ -19,18 +19,18 @@ func main() {
 		fmt.Printf(`"%v" is not a valid year`, s)
 		return
 	}
-	//if year%4 == 0 && year%100 != 0 || year%400 == 0 {
+	/*
+		var leap bool
+		if year%400 == 0 {
+			leap = true
+		} else if year%100 == 0 {
+			leap = false
+		} else if year%4 == 0 {
+			leap = true
+		}
+	*/
 
-	var leap bool
-	if year%400 == 0 {
-		leap = true
-	} else if year%100 == 0 {
-		leap = false
-	} else if year%4 == 0 {
-		leap = true
-	}
-
-	if leap {
+	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
 		fmt.Printf("%d is a leap year", year)
 	} else {
 		fmt.Printf("%d is not a leap year", year)
